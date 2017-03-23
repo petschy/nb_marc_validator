03116d5c2e5436f9766d5516196f7c3f5eed8a95
petschy <peter@calamo.ch>
2017-03-23 09:31:59 +0100

    Add Warnings
    - write headings to file

--- validate.pl
+++ validate.pl
@@ -8,3 +8,13 @@ use diagnostics;
 use MARC::File::USMARC;
 use MARC::Record;
 use MARC::Field;
+
+# Warnings
+my $bib_id = "BIB_ID";
+my $tag = "TAG";
+my $ind_or_sf = "IND/SF";
+my $problem = "PROBLEM";
+my @warnings = ($bib_id, $tag, $ind_or_sf, $problem);
+my $fh_warnings = IO::File->new("warnings.txt", "w");
+say $fh_warnings join "\t", @warnings; # Write headings to warnings file
+
