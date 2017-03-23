8082ff9098f477c3ef3f1aa52d90a6773369e32f
petschy <peter@calamo.ch>
2017-03-23 16:59:14 +0100

    Typo

--- validate.pl
+++ validate.pl
@@ -34,14 +34,14 @@ my $problem   = "PROBLEM";
 my @warnings;
 my @warnings_heading = ( $bib_id, $tag, $ind_or_sf, $problem );
 my $fh_warnings = IO::File->new( $rule . "_warnings.txt", '>:utf8' );
-say $fh_warnings join "\t", @warnings_heading; 
+say $fh_warnings join "\t", @warnings_heading;
 
 my $i       = 1;
 my $records = MARC::File::USMARC->in($marcfile);
 while ( my $record = $records->next() )
 {
 
-	my $bib_id = "${\$rule}BibId"->new( warnings => \@warnings );
+	my $bib_id = "${\$rule}BibId"->new();
 	@warnings = $bib_id->set_bib_id( $record->field('001')->as_string() );
 	say "Record no $i: BibId " . $bib_id->bib_id;
 	if (@warnings) { say $fh_warnings join "\t", @warnings; }
