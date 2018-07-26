be2b3944d4b2c685e4d64705b0abb725bcce5997
petschy <peter@calamo.ch>
2018-07-26 17:07:08 +0200

    F006.pm
    - Clean up

--- lib/F006.pm
+++ lib/F006.pm
@@ -37,7 +37,7 @@ sub _codes
 	my $self     = shift;
 	my $config   = MyConfig->new();
 	my $datafile = $config->datadir() . $self->data_file;
-	my $fh = IO::File->new( $datafile, '<:utf8' );
+	my $fh       = IO::File->new( $datafile, '<:utf8' );
 	if ( defined $fh )
 	{
 		my %table;
@@ -61,7 +61,7 @@ sub _positions
 	my $self     = shift;
 	my $config   = MyConfig->new();
 	my $datafile = $config->datadir() . $self->data_file . "_POSITIONS";
-	my $fh = IO::File->new( $datafile, '<:utf8' );
+	my $fh       = IO::File->new( $datafile, '<:utf8' );
 	if ( defined $fh )
 	{
 		my %table;
@@ -117,37 +117,6 @@ sub _form_of_material
 
 }
 
-sub _values
-{
-	my $self = shift;
-	( my $type_of_material, my $position ) = @_;
-	my %table = %{ $self->codes };
-
-	say $type_of_material;
-	say $position;
-
-	#	foreach my $column_1 ( keys %table )
-	#	{
-	#		foreach my $column_2 ( keys %{ $table{$column_1} } )
-	#		{
-	#			say "$column_1 - $column_2 = ", $table{$column_1}{$column_2};
-	#			say "$column_1 - $column_2 = ", join ', ',
-	#			  ( @{ $table{$column_1}{$column_2} } );
-	#			foreach my $third ( @{ $table{$column_1}{$column_2} } )
-	#			{
-	#				#					say $third;
-	#			}
-	#
-	#			#				say Dumper $table{$column_1}{$column_2};
-	#			my $default_values = join ', ',
-	#			  ( @{ $table{$column_1}{$column_2} } );
-	#
-	#			#				say $default_values;
-	#
-	#		}
-	#	}
-	return @{ $table{$type_of_material}{$position} };
-}
 
 sub check
 {
@@ -155,12 +124,12 @@ sub check
 	my ( $bib_id, $field, $warnings ) = @_;
 
 	my $type_of_material = _form_of_material( substr( $field->data(), 0, 1 ) );
-	my %codes                = %{ $self->codes };
-	my %positions            = %{ $self->positions };
-	my $tag                  = $field->tag();
-	my $default_length       = 18;
-	my $code                 = '-';
-	my $default_values       = '-';
+	my %codes            = %{ $self->codes };
+	my %positions        = %{ $self->positions };
+	my $tag              = $field->tag();
+	my $default_length   = 18;
+	my $code             = '-';
+	my $default_values   = '-';
 	my @default_values_array = ();
 	my $pos                  = '-';
 	my $length               = length( $field->data() );
@@ -190,31 +159,40 @@ sub check
 			{
 				$code = substr( $field->data(), $i, $offset );
 				$code =~ s/ /#/g;
-				$code = quotemeta $code;
-				say $type_of_material;
-				say $pos;
-				say Dumper @{ $codes{$type_of_material}{$pos} };
 				@default_values_array = @{ $codes{$type_of_material}{$pos} };
 				$default_values = join( ", ", @default_values_array );
-				say $default_values;
-
-				unless ( grep( /$code/, @default_values_array ) )
+				if ( $type_of_material eq "VM" && $pos eq "01-03" )
 				{
-					$code = unquotemeta($code);
-					my @message = (
+					my $pattern = '^[\d]{3}$|^\|{3}$|^\-{3}$|^n{3}$';
+					unless ( $code =~ $pattern )
+					{
+						my @message = (
 								  $bib_id, $tag,            $pos,
 								  $code,   $default_values, $length,
 								  $default_length, $length_ok, $type_of_material
-					);
-					$warnings->add_warning( \@message );
+						);
+						$warnings->add_warning( \@message );
+					}
+
+				} else
+				{
+					$code = quotemeta $code;
+					unless ( grep( /$code/, @default_values_array ) )
+					{
+						$code = unquotemeta($code);
+						my @message = (
+								  $bib_id, $tag,            $pos,
+								  $code,   $default_values, $length,
+								  $default_length, $length_ok, $type_of_material
+						);
+						$warnings->add_warning( \@message );
+					}
 				}
 			}
 		}
 
 	}
 
-#	@default_values_array = &_values( $self, $type_of_material, '05' );
-#	say join ', ', @default_values_array;
 }
 
 __PACKAGE__->meta->make_immutable;
