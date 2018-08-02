66c1b90d5e40ee5114afafb18f0582c3017de65e
petschy <peter@calamo.ch>
2018-08-02 07:48:47 +0200

    F006.pm
    - check pos 00

--- lib/F006.pm
+++ lib/F006.pm
@@ -32,6 +32,14 @@ has 'positions' => (
 					 builder => '_positions'
 );
 
+has 'form_of_material' => (
+	is  => 'ro',
+	isa => 'HashRef',
+
+	#					 lazy    => 1,
+	builder => '_set_form_of_material'
+);
+
 sub _codes
 {
 	my $self     = shift;
@@ -80,64 +88,84 @@ sub _positions
 	}
 }
 
-sub _form_of_material
+sub _set_form_of_material
 {
-	my $form = shift;
-	if ( $form eq 'a' || $form eq 't' )
+	my $self     = shift;
+	my $code     = shift;
+	my $config   = MyConfig->new();
+	my $datafile = $config->datadir() . $self->data_file . "_FORM_OF_MATERIAL";
+	my $fh       = IO::File->new( $datafile, '<:utf8' );
+	if ( defined $fh )
 	{
-		return 'BK';
-	} elsif ( $form eq 'm' )
-	{
-		return 'CF';
-	} elsif ( $form eq 'e' || $form eq 'f' )
-	{
-		return 'MP';
-	} elsif (    $form eq 'c'
-			  || $form eq 'd'
-			  || $form eq 'i'
-			  || $form eq 'j' )
-	{
-		return 'MU';
-	} elsif (    $form eq 'g'
-			  || $form eq 'k'
-			  || $form eq 'o'
-			  || $form eq 'r' )
-	{
-		return 'VM';
-	} elsif ( $form eq 'p' )
-	{
-		return 'MX';
-	} elsif ( $form eq 's' )
-	{
-		return 'CR';
+		my %table;
+		while ( my $row = $fh->getline )
+		{
+			chomp $row;
+			my ( $key, $value ) = split /\t/, $row;
+			$table{$key} = $value;
+		}
+		undef $fh;
+		return \%table;
 	} else
 	{
-		return 'INVALID';
+		say "Die Datei $datafile ist nicht vorhanden.";
+		exit;
 	}
-
 }
 
+sub _get_form_of_material
+{
+	my $self = shift;
+	my $code = shift;
+	my %hash = %{ $self->form_of_material };
+	return $hash{$code};
+}
 
 sub check
 {
 	my $self = shift;
 	my ( $bib_id, $field, $warnings ) = @_;
 
-	my $type_of_material = _form_of_material( substr( $field->data(), 0, 1 ) );
-	my %codes            = %{ $self->codes };
-	my %positions        = %{ $self->positions };
-	my $tag              = $field->tag();
-	my $default_length   = 18;
-	my $code             = '-';
-	my $default_values   = '-';
+#	my $type_of_material = _get_form_of_material ($self, substr( $field->data(), 0, 1 ) );
+
+	my %codes                = %{ $self->codes };
+	my %positions            = %{ $self->positions };
+	my $tag                  = $field->tag();
+	my $default_length       = 18;
+	my $code                 = substr( $field->data(), 0, 1 );
+	my $default_values       = '-';
 	my @default_values_array = ();
 	my $pos                  = '-';
 	my $length               = length( $field->data() );
 	my $length_ok            = "JA";
 
+	my $type_of_material = "INVALID";
+	my %form_of_material = %{ $self->form_of_material };
+	if ( $form_of_material{$code} )
+	{
+		$type_of_material = $form_of_material{$code};
+	} else
+	{
+		foreach my $key ( keys %form_of_material )
+		{
+			push @default_values_array, $key;
+		}
+		$default_values = join( ", ", sort @default_values_array );
+		$pos = "00";
+		unless ( $length == $default_length ) {$length_ok = "NEIN"};
+		my @message = (
+						$bib_id,         $tag,            $pos,
+						$code,           $default_values, $length,
+						$default_length, $length_ok,      $type_of_material
+		);
+		$warnings->add_warning( \@message );
+		return;
+	}
+
 	# Length of field
 	unless ( $length == $default_length )
 	{
+		$code      = '-';
 		$length_ok = 'NEIN';
 		my @message = (
 						$bib_id,         $tag,            $pos,
